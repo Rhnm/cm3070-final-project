fb4aaf98055295080b0f1217c8516f850d1f1c00
Rhnm <rhnmaheshwari@gmail.com>
2024-08-12 17:23:33 +0530

    updated index.js for inserting data to database and updated database for tables taskcategory and taskpriority

---
+++ backend/database/insert.sql
@@ -0,0 +1,17 @@
+INSERT INTO TaskCategory (name) VALUES
+('Personal'),
+('Professional'),
+('Other');
+
+INSERT INTO TaskStatus (name) VALUES
+('Pending'),
+('In Progress'),
+('Completed'),
+('On Hold'),
+('Cancelled');
+
+
+INSERT INTO TaskPriority (name) VALUES
+('High'),
+('Medium'),
+('Low');
