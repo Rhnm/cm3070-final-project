dd871a39987a0fba5051aca5dbfd0aa5bcba5abf
Rhnm <rhnmaheshwari@gmail.com>
2024-08-13 17:30:21 +0530

    updated database

--- frontend/apiConfig.js
+++ frontend/apiConfig.js
@@ -1,3 +1,3 @@
-const ipAddress = '192.168.1.11';//'192.168.0.2'; // Replace with your actual IP address
+const ipAddress = '192.168.1.15'; // Replace with your actual IP address
 
 export const baseURL = `http://${ipAddress}`;
