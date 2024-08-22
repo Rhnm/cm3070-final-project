8b2e31242439aaa04a8f92a19a2446ac038c5d6c
Rhnm <rhnmaheshwari@gmail.com>
2024-08-22 17:14:51 +0530

    fixed iphone layout for picker in newtaskscreen

--- frontend/screens/NewTaskScreen.js
+++ frontend/screens/NewTaskScreen.js
@@ -260,6 +260,8 @@ const styles = StyleSheet.create({
     borderColor: '#ccc',
     borderRadius: 5,
     width: '100%',
+    height: 200,
+    marginBottom: 10,
   },
   picker: { 
     height: 50, 
